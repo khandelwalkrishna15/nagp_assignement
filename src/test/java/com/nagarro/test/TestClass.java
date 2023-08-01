2166f2f735ee14dbc44882cac59e1aa41a698666
Krishna Khandelwal <krishna.khandelwal@nagarro.com>
2023-08-01 22:50:57 +0530

    first commit

---
+++ src/test/java/com/nagarro/test/TestClass.java
@@ -0,0 +1,22 @@
+package com.nagarro.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestClass{
+
+	@Test
+	public void TestCase1() {
+	
+	System.out.println("Test Case 1");
+	Assert.assertEquals(true, true  );
+}
+	@Test
+	public void TestCase2() {
+	
+	System.out.println("Test Case 2");
+	Assert.assertEquals(true, true  );
+}		
+			
+}
